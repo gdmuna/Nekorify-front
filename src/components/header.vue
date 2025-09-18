acca2f4a10910d6085c090d4ff0549a8f5e175ee
FOV-RGT <Q1950088436@outlook.com>
2025-09-18 22:21:52 +0800

    style: 发生甚么事了

--- src/components/header.vue
+++ src/components/header.vue
@@ -41,7 +41,7 @@
         </div>
         <header
             ref="headerRef"
-            class="flex lg:flex-row flex-col lg:h-full h-screen lg:w-full w-[min(24rem,70dvw)] -translate-x-full lg:-translate-x-0 relative lg:items-center items-start lg:justify-between justify-start *:shrink-0 z-60 space-y-4 lg:space-y-0 will-change-transform overscroll-contain overflow-x-auto *:pointer-events-auto -top-14 lg:-top-0 -left-4 lg:-left-0 lg:bg-transparent bg-[#0E100F]">
+            class="flex lg:flex-row flex-col lg:h-full h-screen lg:w-full w-[min(24rem,70dvw)] -translate-x-full lg:-translate-x-0 relative lg:items-center items-start lg:justify-between justify-start *:shrink-0 z-60 space-y-4 lg:space-y-0 will-change-transform overscroll-contain *:pointer-events-auto -top-14 lg:-top-0 -left-4 lg:-left-0 lg:bg-transparent bg-[#0E100F]">
             <!-- 页眉左侧内容 -->
             <div class="flex lg:flex-row flex-col lg:h-full lg:items-center lg:w-auto w-full space-y-4 lg:space-y-0">
                 <!-- LOGO -->
